aef5c079f69bf868cee6012552bbd15647d6aa9c
Henrik-3 <henrikmertens3@gmail.com>
2022-06-17 12:02:22 +0200

    Add paramter validation

--- package/src/index.js
+++ package/src/index.js
@@ -18,7 +18,14 @@ class HenrikDevValorantAPI {
                 reset: Number(req.response ? req.response.headers['x-ratelimit-reset'] : req.headers['x-ratelimit-reset']),
             },
             error: req.response ? this._parsebody(req.response.data) : null,
+            url: req.config.url,
         };
+    }
+    _validate(input) {
+        for (let i = 0; Object.values(input).length > i; i++) {
+            console.log(Object.values(input)[i]);
+            if (!Object.values(input)[i]) throw new Error(`Missing parameter: ${Object.keys(input)[i]}`);
+        }
     }
     async _fetch({url, type} = {}) {
         const req = await axios({
@@ -27,17 +34,23 @@ class HenrikDevValorantAPI {
             headers: this.token
                 ? {
                       Authentication: this.token,
+                      'User-Agent': 'unofficial-valorant-api/node.js/2.1',
                   }
                 : null,
         }).catch(err => err);
         return this._parseresponse(req);
     }
 
-    async getAccount({name, tag} = {}) {
-        return await this._fetch({url: `https://api.henrikdev.xyz/valorant/v1/account/${encodeURI(name)}/${encodeURI(tag)}`, type: 'GET'});
+    async getAccount({name, tag, force} = {}) {
+        this._validate({name, tag});
+        return await this._fetch({
+            url: `https://api.henrikdev.xyz/valorant/v1/account/${encodeURI(name)}/${encodeURI(tag)}${force ? `?force=${true}` : ''}`,
+            type: 'GET',
+        });
     }
 
     async getMMRByPUUID({version, region, puuid, filter} = {}) {
+        this._validate({version, region, puuid});
         return await this._fetch({
             url: `https://api.henrikdev.xyz/valorant/${version}/by-puuid/mmr/${region}/${puuid}${filter ? `?filter=${filter}` : ''}`,
             type: 'GET',
